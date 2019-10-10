e11b309a3b8287cf17fcd6876ffbf882f93d6953
JaMo42 <30950798+JaMo42@users.noreply.github.com>
2019-10-10 11:17:23 +0200

    added doc comments

--- Source/Options.h
+++ Source/Options.h
@@ -10,12 +10,25 @@ struct Option {
 };
 
 namespace {
+    // Holds all arguments added by AddArgument
 	std::vector<Option> Options;
+    // Contains all flags for the arguments, used by GetOpts
 	tstring Flags;
 }
 
+/**
+ * @brief Adds a commandline option.
+ * @param Opt - The option to add.
+ */
 void AddArgument(Option &&Opt);
 
+/**
+ * @brief Applies all options given in a commandline argument.
+ * For each given flag, sets the target of the option to its specified value.
+ */ 
 void GetOpts(const TCHAR *Argument);
 
+/**
+ * @brief Displays the help text.
+ */ 
 void Help();
