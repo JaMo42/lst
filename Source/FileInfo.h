e92c1ff09e58403a2e59cc0fa411989e805640ac
JaMo42 <30950798+JaMo42@users.noreply.github.com>
2019-12-12 08:29:19 +0100

    add doc comments

--- Source/FileInfo.h
+++ Source/FileInfo.h
@@ -2,17 +2,31 @@
 #include "Unicode.h"
 #include "Filetype.h"
 
-
+/**
+ * Holds file information.
+ */
 struct FileInfo {
+  // The handle of the file
 	HANDLE Handle;
+  // The file type
 	FILETYPE Type;
+  // The name of the file
 	tstring Name;
+  // For symlinks, the name of the linked file
 	tstring LinkName;
+  // For symlinks, whether to linked file exists
 	bool LinkOK;
+  // The creation time
 	FILETIME Creation;
+  // The time of the last modification
 	FILETIME LastWrite;
+  // The size of the file
 	DWORD Size;
+  // Whether the file is hidden
 	bool Hidden;
 
+  /**
+   * @param FindData - WIN32_FIND_DATA structure to get file information from.
+   */
 	FileInfo(const WIN32_FIND_DATA &FindData);
 };
